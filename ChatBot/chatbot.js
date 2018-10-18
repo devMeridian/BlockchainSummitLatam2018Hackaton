6bbc455ad112cd182732bfcf94bf078885eaaeb2
saraujoc <saraujoc@unal.edu.co>
2018-10-18 11:30:15 -0500

    kovalt socket

--- ChatBot/chatbot.js
+++ ChatBot/chatbot.js
@@ -9,6 +9,14 @@ const telegram = new Telegram("537420063:AAGOls9AjwY2emUTwx3RttcKtgbzCy5P6BU");
 // set your web3 object
 var web3 = new Web3();
 
+web3.setProvider(
+  new web3.providers.WebsocketProvider("ws://kovan.infura.io/v3/ws")
+);
+
+// const EthereumTx = require('ethereumjs-tx')
+// const privateKey = Buffer.from("0f81a091c8d9af7295c5138f9c106292dc015d1e67e9a909275ab1adf3ced9b7", 'hex')
+// const address_origin = '0xaa4f57eeb8c7c4d49701b51a00eb7a641f36c8ce';
+
 chatId = 0;
 
 function registerSession(ctx) {
@@ -33,7 +41,8 @@ bot.on("sticker", ctx => ctx.reply("👍"));
 bot.startPolling();
 
 // set the web3 object local blockchain node
-web3.setProvider(new web3.providers.WebsocketProvider("ws://localhost:8545"));
+// web3.setProvider(new web3.providers.WebsocketProvider("ws://localhost:8545"));
+// web3.setProvider(new web3.providers.WebsocketProvider("ws://kovan.infura.io/v3/"));
 // log some web3 object values to make sure we're all connected
 console.log(web3.version.api);
 
@@ -73,10 +82,11 @@ var ABI = [
     type: "function"
   }
 ];
-var trace_contract = "0xaa4f57eeb8c7c4d49701b51a00eb7a641f36c8ce";
+var address = "0xac133e2580dbbabcbdc8266a8702a5df25a4e934";
+// var trace_contract = "0xaa4f57eeb8c7c4d49701b51a00eb7a641f36c8ce";
 
 // now retrieve your contract object with the ABI and contract address values
-var trace = new web3.eth.Contract(ABI, trace_contract);
+var trace = new web3.eth.Contract(ABI, address);
 
 // indefinite recursive loop to read the 'ItBlinks' event in the blink contract.
 var event = trace.events
