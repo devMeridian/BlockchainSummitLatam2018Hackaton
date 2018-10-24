52117cdb60186db08e6f2dfd36661de3eb7c9e4f
RafaSaavedra <rafa.saavedra@gmail.com>
2018-10-23 23:11:25 -0700

    Adding full set of files for submission

---
+++ SContracts/SupplyProgress.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.24;
+
+import "sai/tub.sol";
+import "ds-math/math.sol";
+import "ds-value/value.sol";
+
+contract SupplyProgress is DSMath {
+
+
+    uint256 public collateral;
+    uint256 public debt;
+    uint256 public price;
+    uint256 public totalValue;
+    uint256 public fee;
+    uint256 public surplus;
+
+    DSToken constant dai = DSToken(0xC4375B7De8af5a38a93548eb8453a498222C4fF2);
+
+    event Owner();
+    event RetrievedData(uint collateral, uint debt, uint price);
+    event Calculated(uint totalValue, uint fee, uint surplus);
+    event Shut();
+    event Transfered();
+    event Funded(address _address, uint amount, uint total);
+    event Paid(address _address, uint amount);
+
+
+    event Supply(bool successful);
+
+    constructor() public {}
+
+    function setSucceeded(bool successful) public {
+        require(dai.transfer(msg.sender, 1));
+        emit Supply(successful);
+    }
+
+
+    function fund(uint amount) public {
+        require(amount > 0);
+        require(dai.transferFrom(msg.sender, address(this), amount));
+    }
+
+}
---
+++ TraceCoffeeBackend/common/models/answer.js
@@ -0,0 +1,95 @@
+"use strict";
+
+var Web3 = require("web3");
+
+var web3 = new Web3();
+web3.setProvider(
+  new web3.providers.WebsocketProvider("ws://kovan.infura.io/v3/ws")
+);
+
+const EthereumTx = require("ethereumjs-tx");
+const privateKey = Buffer.from(
+  "0f81a091c8d9af7295c5138f9c106292dc015d1e67e9a909275ab1adf3ced9b7",
+  "hex"
+);
+const address_origin = "0xaa4f57eeb8c7c4d49701b51a00eb7a641f36c8ce";
+
+var ABI = [
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "successful",
+        type: "bool"
+      }
+    ],
+    name: "SupplyProgress",
+    type: "event"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "successful",
+        type: "bool"
+      }
+    ],
+    name: "setSucceeded",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  }
+];
+
+async function callContract(hasSucceeded) {
+  var address = "0xac133e2580dbbabcbdc8266a8702a5df25a4e934";
+  var count = web3.eth.getTransactionCount(address_origin);
+  var trace_contract = new web3.eth.Contract(ABI, address);
+
+  let data = trace_contract.methods.setSucceeded(hasSucceeded).encodeABI();
+  // .send({ from: "0x082376777B71F3897b68552C759d8f09D9DD1a1C" });
+
+  const txParams = {
+    nonce: count,
+    gasPrice: 22000000000,
+    gasLimit: 4300000,
+    to: trace_contract,
+    value: "0x00",
+    data: data,
+    chainId: 4
+  };
+  const tx = new EthereumTx(txParams);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+  web3.sendSignedTransaction("0x" + serializedTx.toString("hex"));
+
+  console.log("contract sent");
+}
+
+module.exports = function(Answer) {
+  Answer.observe("before save", function(ctx, next) {
+    console.log(ctx.methodString, " was invoked remotely *************");
+
+    var answer = ctx.instance;
+
+    var hasSucceeded = true;
+    var { answers } = answer;
+    console.log("answers: ", answers);
+    answers.forEach(answerData => {
+      console.log("validating answer: ", answerData);
+      if (answerData["questionId"].endsWith("Quality"))
+        if (answerData.response[0] != "Verdadero") hasSucceeded = false;
+    });
+
+    callContract(hasSucceeded);
+    next();
+  });
+};
